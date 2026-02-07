29c851a084984bd43e23def0c48f3b9f0a03356b
bhoom007 <155531850+bhoom007@users.noreply.github.com>
2026-02-07 20:15:46 +0530

    Add files via upload

---
+++ measurements.py
@@ -0,0 +1,12 @@
+class Measurements:
+
+    def area(self, length, width):
+        return length * width
+
+    def perimeter(self, length, width):
+        return 2 * (length + width)
+
+
+        
+        
+    
