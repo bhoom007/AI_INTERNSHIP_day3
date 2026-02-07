113119bf9e827523d08124b65eab2d78fc81e463
bhoom007 <155531850+bhoom007@users.noreply.github.com>
2026-02-07 22:11:34 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,11 @@
+import math_operations
+
+
+power_result = math_operations.power(2, 10)
+print("2 raised to power 10 is:", power_result)
+
+numbers = [10, 20, 30, 40]
+
+
+avg_result = math_operations.average(numbers)
+print("Average of the numbers is:", avg_result)
---
+++ math_operations.py
@@ -0,0 +1,14 @@
+def power(base, exp):
+    return base ** exp
+
+
+
+def average(numbers_list):
+    total = 0
+
+    
+    for num in numbers_list:
+        total = total + num
+
+
+    return total / len(numbers_list)
